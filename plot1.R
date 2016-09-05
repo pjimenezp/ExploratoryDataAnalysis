50ece819c20e63bee33cd4b0ac1492b3578d94a6
Priscilla Jimenez <pjimen5@uic.edu>
2016-09-04 21:50:44 -0500

    Plot1

--- plot1.R
+++ plot1.R
@@ -4,3 +4,9 @@ powerConsumption$Date<-as.Date(powerConsumption$Date,format="%d/%m/%Y")
 # Selecting only specific days 
 pc <- powerConsumption[powerConsumption$Date>="2007-02-01" & powerConsumption$Date<="2007-02-02",]
 
+## Plot 1
+pc$Global_active_power<-as.numeric(pc$Global_active_power)
+
+png("plot1.png",480,480)
+hist(pc$Global_active_power,breaks=15,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
