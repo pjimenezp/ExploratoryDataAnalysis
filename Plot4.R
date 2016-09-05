9b299aa7a2bad10c1ef22a9f93aaf48eb86c7e2f
Priscilla Jimenez <pjimen5@uic.edu>
2016-09-04 22:52:12 -0500

    Plot4

--- Plot4.R
+++ Plot4.R
@@ -4,16 +4,20 @@ powerConsumption$Date<-as.Date(powerConsumption$Date,format="%d/%m/%Y")
 # Selecting only specific days 
 pc <- powerConsumption[powerConsumption$Date>="2007-02-01" & powerConsumption$Date<="2007-02-02",]
 
-## Plot 3
+## Plot 4
 pc$Global_active_power<-as.numeric(pc$Global_active_power)
 pc$DateTime <- strptime(paste(pc$Date, pc$Time), "%Y-%m-%d %H:%M")
 
-png("plot3.png",480,480)
+png("plot4.png",480,480)
+par(mfrow=c(2,2))
+
+plot(pc$DateTime,pc$Global_active_power,type="l",xlab="Date",ylab="Global Active Power")
+
+plot(pc$DateTime,pc$Voltage,type="l",xlab="Date",ylab="Voltage")
 
 plot(pc$DateTime,pc$Sub_metering_1,type="l",xlab="Date",ylab="Energy sub metering")
 lines(pc$DateTime,pc$Sub_metering_2,col="red",lwd=1)
 lines(pc$DateTime,pc$Sub_metering_3,col="blue",lwd=1)
-
 legend("topright",
        inset=0,
        cex = 1,
@@ -24,6 +28,8 @@ legend("topright",
        col=c("black","red","blue"),
        bg="grey96",
        text.font=3)
+       
+plot(pc$DateTime,pc$Global_reactive_power,type="l",xlab="Date",ylab="Global reactive power")    
        
 dev.off()
 
